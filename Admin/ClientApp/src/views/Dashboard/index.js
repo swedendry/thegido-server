c9c3f5bc7d46a5e963113b3ace4e6e017fb04218
thegido <thegido@wisecat.co.kr>
2020-04-10 16:52:15 +0900

    dashboard

--- Admin/ClientApp/src/views/Dashboard/index.js
+++ Admin/ClientApp/src/views/Dashboard/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { IoLogoGithub, IoIosSearch, IoIosRocket } from 'react-icons/io';
+import { IoLogoGithub, IoIosRocket } from 'react-icons/io';
 import DashboardCard from '../../components/Common/DashboardCard';
 
 export default function Main() {
@@ -7,20 +7,20 @@ export default function Main() {
         {
             backgroud: 'linear-gradient(to right, #6190E8 100%, #A7BFE8 80%, #6190E8 100%)',
             icon: <IoLogoGithub size={60} color="#fff" />,
-            description: 'Source(Github)',
-            url: 'https://github.com/swedendry/Server',
+            description: 'ClientSource(Github)',
+            url: 'https://github.com/swedendry/thegido-client',
         },
         {
-            backgroud: 'linear-gradient(to right, #614385 0%, #516395 100%, #614385 100%)',
-            icon: <IoIosSearch size={60} color="#fff" />,
-            description: 'Log(Kibana)',
-            url: 'http://52.231.118.142:5601/',
+            backgroud: 'linear-gradient(to right, #6190E8 100%, #A7BFE8 80%, #6190E8 100%)',
+            icon: <IoLogoGithub size={60} color="#fff" />,
+            description: 'ServerSource(Github)',
+            url: 'https://github.com/swedendry/thegido-server',
         },
         {
             backgroud: 'linear-gradient(to right, #ff7e5f 0%, #feb47b 100%, #ff7e5f 100%)',
             icon: <IoIosRocket size={60} color="#fff" />,
             description: 'Api(Swagger)',
-            url: 'http://52.231.118.149:80/swagger/index.html',
+            url: 'https://thegido-admin.azurewebsites.net/swagger/index.html',
         },
     ];
 
